bac1478021f227f4384c291b76634c59ddd304c8
Pravin Jadhav <pravinjadhav762@gmail.com>
2017-05-23 11:48:06 +0530

    data

---
+++ protracter_conf.js
@@ -0,0 +1,22 @@
+// An example configuration file.
+exports.config = {
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+  baseUrl:'http://localhost:3000',
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['test/*.spec.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
